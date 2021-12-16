a565d05f7e4ea5e75da39bfb6d6ff39715738423
Micheal Limones <micheal@zestylimones.com>
2021-12-16 11:25:12 -0700

    updated route display on app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NaveMenu from './components/NavMenu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
 
 export default function App() {
   return (
     <Router>
       <NaveMenu />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
   );
 }
