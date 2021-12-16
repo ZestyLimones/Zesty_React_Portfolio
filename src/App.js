de6dcff86af822fa4432811d7b1964d9adeec32a
Micheal Limones <micheal@zestylimones.com>
2021-12-16 10:45:47 -0700

    updated nav menu.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import NaveMenu from './components/NavMenu';
 import { BrowserRouter as Router } from 'react-router-dom';
+import NaveMenu from './components/NavMenu';
 
 export default function App() {
   return (
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default function NaveBar() {
+export default function NavMenu() {
   return (
     <div>
       <ul>
