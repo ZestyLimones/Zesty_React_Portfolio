442aded9cfe0bf1070eb26e6d795e35925785eb7
Micheal Limones <micheal@zestylimones.com>
2021-12-16 13:20:38 -0700

    removed images.

---
+++ src/components/HomeSection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function HomeSection() {
+  return (
+    <div>
+      <div className="container">
+        <h1 className="hero__heading">
+          <span>Hello this is</span>
+          <span>Micheal</span>
+        </h1>
+        <div className="hero__img">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
