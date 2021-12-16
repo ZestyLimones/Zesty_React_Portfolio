035b7f4064c2c95acb3f95285254c11a93e78505
Micheal Limones <micheal@zestylimones.com>
2021-12-16 12:10:33 -0700

    added styling to nav menu.

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavMenuStyles = styled.div`
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 1rem 0;
+  background: var(--dark-bg);
+  ul {
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 90%;
+    text-align: center;
+    li {
+      display: inline-block;
+      border-radius: 8px;
+      transition: 0.3s ease brackgound-color;
+      &:hover {
+        background-color: var(--deep-dark);
+      }
+    }
+    a {
+      display: inline-block;
+      font-family: 'RobotoMono Regular';
+      padding: 1rem 2rem;
+      font-size: 2rem;
+      color: var(--gray-1);
+      outline: none;
+    }
+    .active {
+      color: var(--white);
+    }
+  }
+`;
 
 export default function NavMenu() {
   return (
-    <div>
+    <NavMenuStyles>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" exact>
+            Home
+          </NavLink>
         </li>
         <li>
           <NavLink to="/about">About</NavLink>
@@ -18,6 +56,6 @@ export default function NavMenu() {
           <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
-    </div>
+    </NavMenuStyles>
   );
 }
