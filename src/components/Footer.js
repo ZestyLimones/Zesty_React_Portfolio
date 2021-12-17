9fcffa2c37c7cc90f39a1a69452f7d48b1e227d2
Micheal Limones <micheal@zestylimones.com>
2021-12-16 20:32:01 -0700

    added column data to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import styled from 'styled-components';
 import FooterCol from './FooterCol';
 import PText from './PText';
 
+const FooterStyles = styled.div`
+  background-color: var(--deep-dark);
+  padding-top: 10rem;
+  .container {
+    display: flex;
+    gap: 3rem;
+  }
+  .footer__col1 {
+    flex: 2;
+  }
+  .footer__col2,
+  .footer__col3,
+  .footer__col4 {
+    flex: 1;
+  }
+  .footer__col1__title {
+    font-size: 3.5rem;
+    margin-bottom: 1rem;
+  }
+  .copyright {
+    background-color: var(--dark-bg);
+    text-align: left;
+    padding: 1rem 0;
+    margin-top: 5rem;
+    .para {
+      margin-left: 0;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      gap: 0rem;
+      & > div {
+        margin-top: 5rem;
+      }
+    }
+    .footer__col1 .para {
+      max-width: 100%;
+    }
+    .copyright {
+      .container {
+        div {
+          margin-top: 0;
+        }
+      }
+    }
+  }
+`;
+
 export default function Footer() {
   return (
-    <div>
+    <FooterStyles>
       <div className="container">
         <div className="footer__col1">
           <h1 className="footer__col1__title">Micheal Limones</h1>
@@ -23,6 +73,6 @@ export default function Footer() {
           <FooterCol />
         </div>
       </div>
-    </div>
+    </FooterStyles>
   );
 }
--- src/components/FooterCol.js
+++ src/components/FooterCol.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function FooterCol() {
+export default function FooterCol({
+  heading = 'col heading',
+  links = [
+    {
+      type: 'Link',
+      title: 'home',
+      path: '/home',
+    },
+    {
+      type: 'Link',
+      title: 'About',
+      path: '/about',
+    },
+  ],
+}) {
   return (
     <div>
-      <h1>This is footer column</h1>
+      <h2 className="heading">{heading}</h2>
+      <ul>
+        {links.map((item, index) => (
+          <li key={index}>
+            {item.type === 'Link' ? (
+              <Link to={item.path}>{item.title}</Link>
+            ) : (
+              <a href={item.path} target="_blank" rel="noreferrer">
+                {item.title}
+              </a>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
