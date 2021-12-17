49da04423916e8d8e5a65188c30662451d3390a5
Micheal Limones <micheal@zestylimones.com>
2021-12-17 08:21:08 -0700

    added styling and props." git push

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import ProjectItem from '../components/ProjectItem';
 import SectionTitle from '../components/SectionTitle';
 import ProjectInfo from '../assets/data/projects';
+import ContactBanner from '../components/ContactBanner';
 
 const ProjectsStyles = styled.div`
-  padding: 10rem 0;
+  padding: 10rem 1rem;
+  .projects__allItems {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 5rem;
+    margin-top: 5rem;
+  }
 `;
 
 export default function Projects() {
-  const [projectData, setProjectData] = useState(ProjectInfo);
-
   return (
     <ProjectsStyles>
       <div className="container">
         <SectionTitle heading="Projects" subheading="Here are my projects" />
       </div>
       <div className="projects__allItems">
-        {projectData.map((item) => (
+        {ProjectInfo.map((item) => (
           <ProjectItem
             key={item.id}
             title={item.name}
@@ -26,6 +31,7 @@ export default function Projects() {
           />
         ))}
       </div>
+      <ContactBanner />
     </ProjectsStyles>
   );
 }
