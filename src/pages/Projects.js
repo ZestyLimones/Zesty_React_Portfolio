1286ce4b86555ec33fa18f87c3e69ffb6a2b84cb
Micheal Limones <micheal@zestylimones.com>
2021-12-17 08:13:29 -0700

    added projects to project page.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import ProjectItem from '../components/ProjectItem';
+import SectionTitle from '../components/SectionTitle';
+import ProjectInfo from '../assets/data/projects';
+
+const ProjectsStyles = styled.div`
+  padding: 10rem 0;
+`;
 
 export default function Projects() {
+  const [projectData, setProjectData] = useState(ProjectInfo);
+
   return (
-    <div>
-      <h1>Project page</h1>
-    </div>
+    <ProjectsStyles>
+      <div className="container">
+        <SectionTitle heading="Projects" subheading="Here are my projects" />
+      </div>
+      <div className="projects__allItems">
+        {projectData.map((item) => (
+          <ProjectItem
+            key={item.id}
+            title={item.name}
+            desc={item.desc}
+            img={item.img}
+          />
+        ))}
+      </div>
+    </ProjectsStyles>
   );
 }
