222421c6da951a77794fcd99c7ff4c3c99fa9f78
Micheal Limones <micheal@zestylimones.com>
2021-12-16 17:55:49 -0700

    added more styling and media queries.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -31,6 +31,43 @@ const AboutSectionStyles = styled.div`
     gap: 2rem;
     margin-top: 2rem;
   }
+  @media only screen and (max-width: 950px) {
+    .aboutSection__left {
+      flex: 4;
+    }
+    .aboutSection__right {
+      flex: 3;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      text-align: center;
+    }
+    .aboutSection__left,
+    .aboutSection__right {
+      width: 100%;
+    }
+    .aboutSection__right {
+      margin-top: 3rem;
+    }
+    .section-title {
+      text-align: center;
+    }
+    .para {
+      margin: 0 auto;
+      margin-top: 2rem;
+    }
+    .aboutSection__buttons {
+      flex-direction: column;
+      gap: 0rem;
+      .button-wrapper,
+      a {
+        width: 100%;
+        text-align: center;
+      }
+    }
+  }
 `;
 
 export default function AboutSection() {
--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SectionTitleStyles = styled.div`
+const SectionTitleStyle = styled.div`
   text-align: center;
   p {
     font-family: 'RobotoMono Regular';
     font-size: 2rem;
   }
   h2 {
-    font-family: 'Montserrat bold';
+    font-family: 'Montserrat Bold';
     font-size: 6rem;
     margin-top: 0.5rem;
     text-transform: uppercase;
   }
   @media only screen and (max-width: 768px) {
+    text-align: center;
     p {
       font-size: 1.2rem;
     }
@@ -24,13 +25,13 @@ const SectionTitleStyles = styled.div`
 `;
 
 export default function SectionTitle({
-  subheading = 'This is subheading',
-  heading = 'this is heading',
+  subheading = 'Need Subheading',
+  heading = 'need heading',
 }) {
   return (
-    <SectionTitleStyles className="sectionTitle">
+    <SectionTitleStyle className="section-title">
       <p>{subheading}</p>
       <h2>{heading}</h2>
-    </SectionTitleStyles>
+    </SectionTitleStyle>
   );
 }
