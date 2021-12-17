9b471d867dde77523a25b2d09a8110a5d69af4f7
Micheal Limones <micheal@zestylimones.com>
2021-12-17 10:14:38 -0700

    added smooth scroll.

--- src/App.js
+++ src/App.js
@@ -6,28 +6,29 @@ import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
+import SmoothScrollbar from './components/SmoothScrollbar';
 
 export default function App() {
   return (
     <Router>
       <NaveMenu />
-      <ScrollToTop />
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-      <Footer />
+      <SmoothScrollbar>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </SmoothScrollbar>
     </Router>
   );
 }
---
+++ src/components/SmoothScrollbar.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import Scrollbar from 'react-smooth-scrollbar';
+
+export default function SmoothScrollbar({ children }) {
+  const ref = useRef(null);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const { scrollbar } = ref.current;
+    scrollbar.setPosition(0, 0);
+  }, [pathname]);
+
+  return <Scrollbar ref={ref}>{children}</Scrollbar>;
+}
