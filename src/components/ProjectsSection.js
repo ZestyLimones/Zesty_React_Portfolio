c864f48cf70da51b670ed3f3a3534e85d412dbcc
Micheal Limones <micheal@zestylimones.com>
2021-12-17 07:06:15 -0700

    created projections section.

---
+++ src/components/ProjectsSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Swiper } from 'swiper/react';
+import SectionTitle from './SectionTitle';
+
+export default function ProjectsSection() {
+  return (
+    <div>
+      <div className="container">
+        <SectionTitle heading="Projects" subheading="Some of my recent work" />
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
 import ContactBanner from '../components/ContactBanner';
+import ProjectsSection from '../components/ProjectsSection';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <ServicesSection />
+      <ProjectsSection />
       <ContactBanner />
     </div>
   );
