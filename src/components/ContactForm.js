1e49854fa66e8a3d3a53595388f81dd331f1bda9
Micheal Limones <micheal@zestylimones.com>
2021-12-17 09:37:34 -0700

    added styling to contact page.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,43 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const ContactFormStyles = styled.form`
+  width: 100%;
+  .form-group {
+    width: 100%;
+    margin-bottom: 2rem;
+  }
+  label {
+    font-size: 1.8rem;
+  }
+  input,
+  textarea {
+    width: 100%;
+    font-size: 2rem;
+    padding: 1.2rem;
+    color: var(--gray-1);
+    background-color: var(--deep-dark);
+    outline: none;
+    border: none;
+    border-radius: 8px;
+    margin-top: 1rem;
+  }
+  textarea {
+    min-height: 250px;
+    resize: vertical;
+  }
+  button {
+    background-color: var(--gray-1);
+    color: var(--black);
+    font-size: 2rem;
+    display: inline-block;
+    outline: none;
+    border: none;
+    padding: 1rem 4rem;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+`;
 
 export default function ContactForm() {
   const [name, setName] = useState('');
@@ -7,7 +46,7 @@ export default function ContactForm() {
 
   return (
     <div>
-      <form>
+      <ContactFormStyles>
         <div className="form-group">
           <label htmlFor="name">
             Your Name
@@ -45,7 +84,7 @@ export default function ContactForm() {
           </label>
         </div>
         <button type="submit">Send</button>
-      </form>
+      </ContactFormStyles>
     </div>
   );
 }
--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -6,7 +6,49 @@ import ContactInfoItem from './ContactInfoItem';
 import SectionTitle from './SectionTitle';
 
 const ContactSectionStyles = styled.div`
-  padding: 10rem 1rem;
+  padding: 10rem 0;
+  .contactSection__wrapper {
+    display: flex;
+    gap: 5rem;
+    margin-top: 7rem;
+    justify-content: space-between;
+    position: relative;
+  }
+  .contactSection__wrapper::after {
+    position: absolute;
+    content: '';
+    width: 2px;
+    height: 50%;
+    background-color: var(--gray-1);
+    left: 50%;
+    top: 30%;
+    transform: translate(-50%, -50%);
+  }
+  .left {
+    width: 100%;
+    max-width: 500px;
+  }
+  .right {
+    max-width: 500px;
+    width: 100%;
+    border-radius: 12px;
+    /* padding-left: 3rem; */
+  }
+  @media only screen and (max-width: 768px) {
+    .contactSection__wrapper {
+      flex-direction: column;
+    }
+    .contactSection__wrapper::after {
+      display: none;
+    }
+    .left,
+    .right {
+      max-width: 100%;
+    }
+    .right {
+      padding: 4rem 2rem 2rem 2rem;
+    }
+  }
 `;
 
 export default function ContactSection() {
